960d0ddf030053be1aed8a3b4ec629895bba6ab2
Aryan Vishwakarma <aryanvishwakarma0291@gmail.com>
2026-08-22 11:24:27 +0530

    pair sum

--- 19 ArrayList/practise.java
+++ 19 ArrayList/practise.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.util.Collections;
+// import java.util.Collections;
 
 public class practise {
     public static void main(String[] args) {
@@ -57,15 +57,15 @@ public class practise {
         // }
 
         // most water contatner problem
-        ArrayList <Integer> list3 = new ArrayList<>();
-        list3.add(8);
-        list3.add(6);
-        list3.add(2);
-        list3.add(5);
-        list3.add(4);
-        list3.add(8);
-        list3.add(3);
-        list3.add(7);
+        // ArrayList <Integer> list3 = new ArrayList<>();
+        // list3.add(8);
+        // list3.add(6);
+        // list3.add(2);
+        // list3.add(5);
+        // list3.add(4);
+        // list3.add(8);
+        // list3.add(3);
+        // list3.add(7);
         // int maxWater = 0 ;
         // for(int i=0; i<list3.size(); i++) {
         //     for(int j=0; j<list3.size(); j++) {
@@ -81,24 +81,71 @@ public class practise {
         // #2pointer approach
        
 
-        int LP = 0; 
-        int RP = list3.size()-1;
-        int maxWater = 0;
+        // int LP = 0; 
+        // int RP = list3.size()-1;
+        // int maxWater = 0;
         
-        while(LP < RP) {
+        // while(LP < RP) {
             
-            int ht = Math.min(list3.get(LP) , list3.get(RP));
-            int wd = RP-LP;
-            int currWater = ht*wd;
-            maxWater = Math.max(currWater, maxWater);
-            if(list3.get(LP) < list3.get(RP)) {
-                LP++;
-            } else {
-                RP--;
-            }
+        //     int ht = Math.min(list3.get(LP) , list3.get(RP));
+        //     int wd = RP-LP;
+        //     int currWater = ht*wd;
+        //     maxWater = Math.max(currWater, maxWater);
+        //     if(list3.get(LP) < list3.get(RP)) {
+        //         LP++;
+        //     } else {
+        //         RP--;
+        //     }
 
+        // }
+        // System.out.println(maxWater);
+
+
+        // most water container 
+         ArrayList <Integer> list = new ArrayList<>();
+         list.add(11);
+         list.add(15);
+         list.add(6);
+         list.add(8);
+         list.add(9);
+         list.add(10);
+
+        int flag = 0;
+        int target = 75;
+        int left = 0;
+        int right = list.size()-1;
+        int n = list.size();
+        for(int i=0; i<list.size(); i++) {
+            for(int j=i+1; j<list.size(); j++) {
+                if(list.get(i) > list.get(j)) {
+                    left = j;
+                    right = i;
+                    break;
+
+                }
+            }
         }
-        System.out.println(maxWater);
+        while (left != right) {
+            if((list.get(left)+list.get(right)) == target) {
+                flag = 1;
+                break;
+            }
+            else if((list.get(left)+list.get(right)) < target) {
+                left = (left+1)%n;
+
+            }
+            else if((list.get(left)+list.get(right)) > target) {
+                right = (n+right-1)%n;
+            }
+            
+        }
+
+
+         if(flag == 1) {
+            System.out.println("yes");
+         } else {
+            System.out.println("no");
+         }
 
 
 
